ca33ffeb96fa2e9347bc4ff46547cd247719c0f8
ansumanpanda <panda.ansuman@gmail.com>
2018-04-14 16:28:58 -0400

    modified index.jsp and added a js function

--- sims/src/main/webapp/index.jsp
+++ sims/src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-<h2>Hello Everyone!</h2>
+<h2>Hello Everyone of this world!</h2>
 </body>
 </html>
--- sims/src/main/webapp/js/main.js
+++ sims/src/main/webapp/js/main.js
@@ -1,3 +1,10 @@
 /**
  * This file contains all the scripts required for SIMS
  */
+
+function a(){
+	/*
+	 * This is function a added in bne_feature_1 branch
+	 * 
+	 */
+}
